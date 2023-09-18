98a317fee28f04fd3bacad998685cda7154a4070
GUEST-1001 <nawatgame@live.com>
2023-09-19 02:23:19 +0700

    update some UI

---
+++ Assets/Script/UI/LookAtCamera.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class LookAtCamera : MonoBehaviour
+{
+    private Camera _cam;
+
+    void Awake()
+    {
+        _cam = Camera.main;    
+    }
+
+    void Update()
+    {
+        transform.eulerAngles = _cam.transform.eulerAngles;    
+    }
+}

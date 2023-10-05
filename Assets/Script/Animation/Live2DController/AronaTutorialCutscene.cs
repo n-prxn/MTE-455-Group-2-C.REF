220da8151bfbcbfe10abcabed80b36d79649d197
GUEST-1001 <nawatgame@live.com>
2023-10-06 06:36:16 +0700

    add arona voice

--- Assets/Script/Animation/Live2DController/AronaTutorialCutscene.cs
+++ Assets/Script/Animation/Live2DController/AronaTutorialCutscene.cs
@@ -12,11 +12,15 @@ public class AronaTutorialCutscene : MonoBehaviour
     public Spine.AnimationState spineAnimationState;
     public Spine.Skeleton skeleton;
 
+    [SerializeField] private AudioSource audioSource;
+    public AudioClip[] studentReactVoices;
+
     [SerializeField] private float duration;
 
     private void Awake()
     {
         Instance = this;
+        audioSource = GameObject.FindGameObjectWithTag("Voice Audio").GetComponent<AudioSource>();
     }
 
     // Start is called before the first frame update
@@ -31,7 +35,7 @@ public class AronaTutorialCutscene : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void ChangeLive2DFace(int dialogueIndex)
@@ -40,18 +44,23 @@ public class AronaTutorialCutscene : MonoBehaviour
         {
             case 0:
                 spineAnimationState.SetAnimation(1, "01", true); //1
+                PlayStudentVoice(0);
                 break;
             case 1:
                 spineAnimationState.SetAnimation(1, "10", true); //2
+                PlayStudentVoice(1);
                 break;
             case 2:
                 spineAnimationState.SetAnimation(1, "11", true); //3
+                PlayStudentVoice(2);
                 break;
             case 3:
                 spineAnimationState.SetAnimation(1, "12", true); //4
+                PlayStudentVoice(3);
                 break;
             case 4:
                 StartCoroutine(ChangeLive2D("04", "25"));
+                StartCoroutine(PlayStudentVoice(4, 5));
                 break;
             default:
                 break;
@@ -63,5 +72,29 @@ public class AronaTutorialCutscene : MonoBehaviour
         spineAnimationState.SetAnimation(1, firstFace, true); //5.1
         yield return new WaitForSeconds(duration);
         spineAnimationState.SetAnimation(1, secondFace, true); //5.2
+    }
+
+    void PlayStudentVoice(int index)
+    {
+        if (studentReactVoices[index] != null)
+        {
+            audioSource.Stop();
+            audioSource.clip = studentReactVoices[index];
+            audioSource.Play();
+        }
+    }
+
+    IEnumerator PlayStudentVoice(int index1, int index2)
+    {
+        if (studentReactVoices[index1] != null && studentReactVoices[index2] != null)
+        {
+            audioSource.Stop();
+            audioSource.clip = studentReactVoices[index1];
+            audioSource.Play();
+            yield return new WaitForSeconds(duration);
+            audioSource.Stop();
+            audioSource.clip = studentReactVoices[index2];
+            audioSource.Play();
+        }
     }
 }

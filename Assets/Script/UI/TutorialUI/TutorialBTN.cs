d7299f6f5bb1deee42e2b535b75051d316b04651
GUEST-1001 <nawatgame@live.com>
2023-10-06 15:05:22 +0700

    test Tutorial system

---
+++ Assets/Script/UI/TutorialUI/TutorialBTN.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TutorialBTN : MonoBehaviour
+{
+    public void ClickTagetBTN()
+    {
+        this.gameObject.SetActive(false);
+    }
+}

e324150e73bb1d5876d902748327f9d74a4c438d
GUEST-1001 <nawatgame@live.com>
2023-10-06 08:01:09 +0700

    update gacha ui

--- Assets/Script/UI/Gacha/GachaUI.cs
+++ Assets/Script/UI/Gacha/GachaUI.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GachaUI : MonoBehaviour
 {
     [SerializeField] GameObject gauranteeButton;
     [SerializeField] TMP_Text currentElephs;
+    [SerializeField] Toggle toggleNextBTN;
     private void OnEnable()
     {
         if (GameManager.Instance.setting.isGuaranteePull)
             gauranteeButton.SetActive(false);
         else
             gauranteeButton.SetActive(true);
+            
+        if (!toggleNextBTN.isOn)
+        {
+            toggleNextBTN.isOn = true;
+        }
     }
 
     private void Update()
